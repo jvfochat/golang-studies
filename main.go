fd4d04a70b0e93d48a54d262d751c903e1157b8d
João Vitor <joaovitorfoca2001@gmail.com>
2022-12-19 14:59:00 -0300

    website monitor

--- main.go
+++ main.go
@@ -2,51 +2,87 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramento = 3
+const delay = 5
 
 func main() {
 
 	displaysIntroduction()
-	displaysMenu()
 
-	aplicacao := leAplicacao()
+	for {
 
-	switch aplicacao {
-	case 0:
-		fmt.Println("home")
-	case 1:
-		fmt.Println("menu")
-	case 2:
-		fmt.Println("close")
-		os.Exit(2)
-	default:
-		fmt.Println("Aplicação não identificada")
-		os.Exit(-1)
+		displaysMenu()
+
+		aplicacao := leAplicacao()
+
+		switch aplicacao {
+		case 1:
+			iniciarMonitorando()
+		case 2:
+			fmt.Println("menu")
+
+		case 0:
+			fmt.Println("close")
+			os.Exit(0)
+		default:
+			fmt.Println("Aplicação não identificada")
+			os.Exit(-1)
+		}
+
 	}
+
 }
 
 func displaysIntroduction() {
 	var email string = "joaovitorfoca2001@gmail.com"
 	var name string = "João Vitor"
-	var birthdate string = "18/05/2001"
-	var passoword int = 1234
+	var years string = "21"
 	var version float32 = 1.0
-	fmt.Println(email, "\nHello Sr(a).", name, "\nBirth Date:", birthdate, "\nPassoword:", passoword)
+	fmt.Println("Email:", email, "\nHello Sr(a)", name, "\nyears", years)
 	fmt.Println("version", version)
 
 }
 
 func displaysMenu() {
-	fmt.Println("0- home")
-	fmt.Println("1- menu")
-	fmt.Println("2- close")
+	fmt.Println("1- monitorando")
+	fmt.Println("2- menu")
+	fmt.Println("0- close")
 }
 
 func leAplicacao() int {
 	var aplicacaoLida int
-	fmt.Scanf("%d", &aplicacaoLida)
+	fmt.Scan(&aplicacaoLida)
 	fmt.Println("A Aplicação selecionada", aplicacaoLida)
+	fmt.Println("")
 
 	return aplicacaoLida
+}
+
+func iniciarMonitorando() {
+	fmt.Println("monitorando")
+	sites := []string{"https://www.facebook.com", "https://www.instagram.com", "https://www.americanas.com.br"}
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("testando site:", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "Foi carregado com sucesso.")
+	} else {
+		fmt.Println("Site:", site, "erro.StatusCode:", resp.StatusCode)
+	}
 }
